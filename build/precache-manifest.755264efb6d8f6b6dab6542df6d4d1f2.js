0771400253081dd2c4ffa8fbc3f4a8d34eb2fe5c
yingjie <gmyjie@gmail.com>
2020-08-22 12:05:38 -0700

    deploy

---
+++ build/precache-manifest.755264efb6d8f6b6dab6542df6d4d1f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05bacd3887bb11e75d69fd17999e001a",
+    "url": "/covid-tracker-reactjs/index.html"
+  },
+  {
+    "revision": "5c50fcabe6a195a3de3a",
+    "url": "/covid-tracker-reactjs/static/css/main.efb96270.chunk.css"
+  },
+  {
+    "revision": "18decf972d6f3b342675",
+    "url": "/covid-tracker-reactjs/static/js/2.b536a310.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid-tracker-reactjs/static/js/2.b536a310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c50fcabe6a195a3de3a",
+    "url": "/covid-tracker-reactjs/static/js/main.a2756581.chunk.js"
+  },
+  {
+    "revision": "a993a4ee8ae80204f529",
+    "url": "/covid-tracker-reactjs/static/js/runtime-main.bcfe0066.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker-reactjs/static/media/image.d7265326.png"
+  }
+]);
