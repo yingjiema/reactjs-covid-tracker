5d90df9b5103ac2f92167db23d272148d9fe9778
yingjie <gmyjie@gmail.com>
2021-10-06 22:47:46 -0700

    Updates

---
+++ precache-manifest.f17e81caf2b71c72e7126c550ab7101b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1780866ec558a96dc277b674a1933505",
+    "url": "/reactjs-covid-tracker/index.html"
+  },
+  {
+    "revision": "77d3006d51c583afd630",
+    "url": "/reactjs-covid-tracker/static/css/main.efb96270.chunk.css"
+  },
+  {
+    "revision": "947f144e038bfe362990",
+    "url": "/reactjs-covid-tracker/static/js/2.a3437494.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/reactjs-covid-tracker/static/js/2.a3437494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77d3006d51c583afd630",
+    "url": "/reactjs-covid-tracker/static/js/main.a6dacc5c.chunk.js"
+  },
+  {
+    "revision": "1d0c94191f95f51bffde",
+    "url": "/reactjs-covid-tracker/static/js/runtime-main.57d9c5d0.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/reactjs-covid-tracker/static/media/image.d7265326.png"
+  }
+]);
